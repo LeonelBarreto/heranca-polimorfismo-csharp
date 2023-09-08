9fb0247842f99b7ccfe77903f91672207c40f2dc
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-09-08 13:53:36 -0300

    resolvido: exercicio 3

---
+++ HerancaEPolimorfismo/Exercicio03/Entities/ImportedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+namespace Exercicio03.Entities
+{
+	class ImportedProduct : Product
+	{
+		public double CustomsFee { get; set; }
+
+		public ImportedProduct()
+		{
+		}
+
+        public ImportedProduct(string name, double price, double customsFee) : base (name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " R$ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " ( Taxa Alfandegária: R$ "
+                + CustomsFee.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+    }
+}
+
